52d47ad95c4f74f97acad67b43aa7c6eeb2ad988
ruve <ruben.vermeulen@sofico.be>
2019-10-25 11:06:56 +0200

    upgrades to latest ngrx best practices
    # Conflicts:
    #	src/modules/statemanagement/actions/options.actions.ts
    #	src/modules/statemanagement/reducers/options.reducer.ts

--- src/modules/statemanagement/reducers/options.reducer.ts
+++ src/modules/statemanagement/reducers/options.reducer.ts
@@ -2,15 +2,24 @@ import { Option } from '../../app/types/option.type';
 import { OptionsActions } from '../actions';
 import { ActionTypes } from '../action-types';
 
-export function optionsReducer(state: Option[] = [], action: OptionsActions): Option[] {
-  switch (action.type) {
-    case ActionTypes.ADD_OPTION:
-      return [...state, {...action.payload.option, isSelected: true}];
-    case ActionTypes.REMOVE_OPTION:
-      return state.filter(option => option.optionId !== action.payload.optionId);
-    case ActionTypes.CLEAR_OPTIONS:
-      return [];
-    default:
-      return state;
-  }
+const initialState: Option[] = [];
+const reducer = createReducer(
+  initialState,
+  on(
+    clearOptionsAction,
+    () => []
+  ),
+  on(
+    addOptionAction,
+    (state, {option}) => [...state, {...option, isSelected: true}]
+  ),
+  on(
+    removeOptionAction,
+    (state, {optionId}) => state.filter(option => option.optionId !== optionId)
+  )
+);
+
+export function optionsReducer(state: Option[] = [], action: Action): Option[] {
+  return reducer(state, action);
 }
+
